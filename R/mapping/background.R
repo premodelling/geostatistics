114166ecc467312e8270e2f342dabcdf22043e9f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-04-28 23:51:51 +0100

    Add R/mapping/background.R

---
+++ R/mapping/background.R
@@ -0,0 +1,48 @@
+# Title     : background.R
+# Objective : Background notes
+# Created by: greyhypotheses
+# Created on: 28/04/2022
+
+
+rm(list = ls())
+
+
+# data
+data(world)
+class(world)
+names(world)
+
+
+# world$geom is a list column
+str(world)
+
+
+# plot
+plot(world)
+
+
+# characteristics
+summary(world[, 'lifeExp'])
+
+
+# subsetting ... note, geometry columns are 'sticky'
+world[1:2, 1:3]
+
+
+# a selection of maps
+plot(world[, c('continent', 'region_un', 'subregion', 'type')])
+plot(world[, 'pop'], main = 'population')
+
+
+# layers
+frame <- world[world$continent == 'Asia', ]
+asia <- sf::st_union(frame)
+plot(world['pop'], main = 'population', reset = FALSE)
+plot(asia, add = TRUE, col = 'red')
+
+
+
+
+
+
+
