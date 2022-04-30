bae8d23b4022c0162fc96fb48060b67d5c5d95e4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-04-30 20:25:27 +0100

    Update R/mapping/objects.R

--- R/mapping/objects.R
+++ R/mapping/objects.R
@@ -62,9 +62,19 @@ unreferenced <- data.frame(longitude = 0.1, latitude = 51.5) %>%
   st_as_sf(coords = c('longitude','latitude')) %>%
   st_buffer(dist = 1)
 
-geographic <- data.frame(longitude = 0.1, latitude = 51.5) %>%
-  st_as_sf(coords = c('longitude','latitude'), crs = 4326) %>%
+geographic.thin <- data.frame(longitude = 0.1, latitude = 51.5) %>%
+  st_as_sf(coords = c('longitude','latitude'), crs = 'EPSG:4326') %>%
+  st_buffer(dist = 1e5, max_cells = 1000)
+
+geographic.jagged <- data.frame(longitude = 0.1, latitude = 51.5) %>%
+  st_as_sf(coords = c('longitude','latitude'), crs = 'EPSG:4326') %>%
+  st_buffer(dist = 1e5, max_cells = 100)
+
+sf::sf_use_s2(use_s2 = FALSE)
+geographic.disabled <- data.frame(longitude = 0.1, latitude = 51.5) %>%
+  st_as_sf(coords = c('longitude','latitude'), crs = 'EPSG:4326') %>%
   st_buffer(dist = 1e5)
+sf::sf_use_s2(use_s2 = TRUE)
 
 projected <-  data.frame(x = 530000, y = 180000) %>%
   st_as_sf(coords = c('x', 'y'), crs = 'EPSG:27700') %>%
