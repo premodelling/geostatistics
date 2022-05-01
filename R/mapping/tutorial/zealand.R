fe7322a75fbb600518cdd04599c3e65666a1a090
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-01 23:37:08 +0100

    Update R/mapping/tutorial/zealand.R

--- R/mapping/tutorial/zealand.R
+++ R/mapping/tutorial/zealand.R
@@ -4,26 +4,40 @@
 # Created on: 01/05/2022
 
 
+# I/O, Borders, File Formats, etc.:https://geocompr.robinlovelace.net/read-write.html
+
 
 rm(list = ls())
 
 
 # data
 f <- system.file('shapes/world.gpkg', package = 'spData')
-world <- read_sf(f, quiet = TRUE)
+
 
 # curious
+world <- read_sf(f, quiet = TRUE)
 read_sf(f, query = 'SELECT * FROM world WHERE FID = 1')
 
-# new zealand
-nz <- read_sf(f, query = 'SELECT * FROM world WHERE name_long = "New Zealand"')
-plot(nz)
 
-nz <- ne_countries(country = 'New Zealand')
+
+# New Zealand: Option I
+#
+nz <- read_sf(f, query = 'SELECT * FROM world WHERE name_long = "New Zealand"')
+plot(nz[, 'name_long'], main = 'New Zealand')
+
+
+
+# New Zealand: Option II
+#
+# ... by default rnaturalearth returns objects of class Spatial*
+# ... the objects can be converted into sf objects via st_as_sf()
+nz <- rnaturalearth::ne_countries(country = 'New Zealand')
 class(nz)
 nz <- st_as_sf(nz)
 class(nz)
-plot(nz[, 'admin'])
+
+# the administrative boundaries
+plot(nz[, 'admin'], main = 'New Zealand')
 
 
 
