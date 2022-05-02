0ce476ba144439ebb0b38388de152d039aad816f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-02 17:40:24 +0100

    Update R/mapping/encoding/interface.R

--- R/mapping/encoding/interface.R
+++ R/mapping/encoding/interface.R
@@ -3,14 +3,14 @@
 # Created by: greyhypotheses
 # Created on: 30/04/2022
 
-source(file = 'R/mapping/io/Geocoding.R')
-source(file = 'R/mapping/io/UTM.R')
+source(file = 'R/mapping/encoding/Geocoding.R')
+source(file = 'R/mapping/encoding/UTM.R')
 
 # addresses of places
-address <- c('Auckland, New Zealand', 'London, United Kingdom')
+address <- c('Auckland, New Zealand', 'London, United Kingdom', 'Yaounde, Cameroon')
 
 # get their longitude & latitude values via a geocoding service
 latest <- AddressGeocoding(address = address)
 
-# determie their UTM values via their longitude & latitude values
+# determine their UTM values via their longitude & latitude values
 latest$utm <- mapply(FUN = UTM, longitude = latest$longitude, latitude = latest$latitude)
