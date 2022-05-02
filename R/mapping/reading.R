ccbe0e2196ad547844bf6be1275f730413dd0b64
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-02 01:07:01 +0100

    Update R/mapping/reading.R

--- R/mapping/reading.R
+++ R/mapping/reading.R
@@ -7,16 +7,42 @@
 rm(list = ls())
 
 
+
 # data
 data(world)
 class(world)
-names(world)
 
 
-# world$geom is a list column
-str(world)
+
+# ... shp
+world <- st_read(system.file('shapes/world.shp', package = 'spData'))
+class(world)
+world <- read_sf(system.file('shapes/world.shp', package = 'spData'))
+class(world)
 
 
-# reading
-initial <- st_read(system.file('shapes/world.shp', package = 'spData'))
-alternative <- read_sf(system.file('shapes/world.shp', package = 'spData'))
+# ... gpkg
+f <- system.file('shapes/world.gpkg', package = 'spData')
+world <- read_sf(f, quiet = TRUE)
+class(world)
+nz <- read_sf(f, query = 'SELECT * FROM world WHERE name_long = "New Zealand"')
+class(nz)
+read_sf(f, query = 'SELECT * FROM world WHERE FID = 1')
+
+
+
+# ... by default rnaturalearth returns objects of class Spatial*
+# ... the objects can be converted into sf objects via st_as_sf()
+nz <- rnaturalearth::ne_countries(country = 'New Zealand')
+class(nz)
+nz <- st_as_sf(nz)
+class(nz)
+
+
+# ... https://github.com/rspatial/geodata/
+Z <- geodata::gadm(country = 'India', level = 1, path = tempdir(), version = '4.0')
+class(Z)
+india <- st_as_sf(Z)
+plot(india[, 'COUNTRY'])
+
+
