de30370c13e26f8917186c90c19c218390445b28
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-02 01:07:18 +0100

    Update R/mapping/tutorial/zealand.R

--- R/mapping/tutorial/zealand.R
+++ R/mapping/tutorial/zealand.R
@@ -4,6 +4,8 @@
 # Created on: 01/05/2022
 
 
+
+# nz: https://geocompr.robinlovelace.net/spatial-operations.html
 # I/O, Borders, File Formats, etc.:https://geocompr.robinlovelace.net/read-write.html
 # tm_layout(): https://www.rdocumentation.org/packages/tmap/versions/3.3-3/topics/tm_layout
 
@@ -12,37 +14,27 @@
 rm(list = ls())
 
 
-# data
-f <- system.file('shapes/world.gpkg', package = 'spData')
 
+# new zealand
+data(spData::nz)
+str(nz)
 
-# curious
-world <- read_sf(f, quiet = TRUE)
-read_sf(f, query = 'SELECT * FROM world WHERE FID = 1')
+data(spData::nz_height)
+str(nz_height)
 
+data(spDataLarge::nz_elev)
+str(nz_elev)
 
-
-# New Zealand: Option I
-#
-nz <- read_sf(f, query = 'SELECT * FROM world WHERE name_long = "New Zealand"')
-plot(nz[, 'name_long'], main = 'New Zealand')
-
-
-
-# New Zealand: Option II
-#
-# ... by default rnaturalearth returns objects of class Spatial*
-# ... the objects can be converted into sf objects via st_as_sf()
-nz <- rnaturalearth::ne_countries(country = 'New Zealand')
-class(nz)
-nz <- st_as_sf(nz)
-class(nz)
 
 # the administrative boundaries
 tm_shape(nz) +
   tm_layout(frame = FALSE) +
-  tm_fill()
+  tm_fill() +
+  tm_borders()
 
+tm_shape(nz) +
+  tm_layout(frame = FALSE) +
+  tm_polygons()
 
 
 
