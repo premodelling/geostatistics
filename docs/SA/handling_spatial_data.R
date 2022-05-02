996d2e289bc642da117aaae74ff593deb5e5e7c7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-02 18:54:47 +0100

    Update docs/SA/handling_spatial_data.R

--- docs/SA/handling_spatial_data.R
+++ docs/SA/handling_spatial_data.R
@@ -1,69 +1,80 @@
 rm(list=ls())
 
 
-rb <- read.csv("data/LiberiaRemoData.csv")
-
+rb <- read.csv(file = 'data/frames/LiberiaRemoData.csv')
 rb$prev <- rb$npos/rb$ntest
 
 
-rb.sf <- st_as_sf(rb,coords = c("utm_x","utm_y"))
+rb.sf <- st_as_sf(rb,coords = c('utm_x', 'utm_y'))
 st_crs(rb.sf) <- 32629
 
-liberia.adm0 <- st_read("docs/SA/Liberia/LBR_adm/LBR_adm0.shp")
+
+
+liberia.adm0 <- st_read('data/shapes/Liberia/LBR_adm/LBR_adm0.shp')
 liberia.adm0 <- st_transform(liberia.adm0,crs=32629)
 
-map0 <- tm_shape(liberia.adm0) + 
+
+map0 <- tm_shape(liberia.adm0) +
   tm_borders(lwd=3) 
 map0
 
-map0+tm_shape(rb.sf)+tm_dots(size=0.5)
 
-Map.with.points <- map0+tm_shape(rb.sf)+
-  tm_bubbles("prev", col = "prev", 
-             border.col = "black",
-             style="fixed", 
-             breaks=seq(0,0.4,0.05),
-             palette="-RdYlBu",
-             title.size="Prevalence", 
+map0 +
+  tm_shape(rb.sf) +
+  tm_dots(size=0.5)
+
+
+Map.with.points <- map0 +
+  tm_shape(rb.sf) +
+  tm_bubbles(size = 'prev', col = 'prev',
+             border.col = 'black',
+             style = 'fixed',
+             breaks = seq(0,0.4,0.05),
+             palette = '-RdYlBu',
+             title.size = 'Prevalence',
              scale = 1,
-             title.col="") 
+             title.col = '')
+Map.with.points
 
 
-Map.with.points+
-  tm_compass(type="8star", 
-             position = c("right","top"))+
-  tm_scale_bar(breaks = c(0,100,200),text.size=1,
-               position=c("center","bottom")) 
+Map.with.points <- Map.with.points +
+  tm_compass(type = '8star', position = c('right', 'top')) +
+  tm_scale_bar(breaks = c(0, 100, 200), text.size = 1, position = c('center', 'bottom'))
+Map.with.points
 
-tmap_mode("view")
+
+tmap_mode(mode = 'view')
+
 
 Map.with.points
-tmap_mode("plot")
 
-liberia.wl <- st_read('docs/SA/Liberia/LBR_wat/LBR_water_lines_dcw.shp')
-liberia.wl <- st_transform(liberia.wl,crs = 32629)
+
+tmap_mode(mode = 'plot')
+
+
+liberia.wl <- st_read('data/shapes/Liberia/LBR_wat/LBR_water_lines_dcw.shp')
+liberia.wl <- st_transform(liberia.wl, crs = 32629)
 
 Map.with.points+tm_shape(liberia.wl)+
-  tm_lines(col="blue", palette="dodgerblue3", 
-           title.col="Waterways")
+  tm_lines(col='blue', palette='dodgerblue3',
+           title.col='Waterways')
 
 ####
-library(raster)
 
-liberia.alt <- raster("Liberia spatial data//LBR_alt/LBR_alt.gri")
+liberia.alt <- raster('Liberia spatial data//LBR_alt/LBR_alt.gri')
 liberia.alt <- projectRaster(liberia.alt,
-               crs=CRS("+init=epsg:32629"))
+               crs=CRS('+init=epsg:32629'))
 
 tm_shape(liberia.alt)+
-tm_raster(title="Elevation")+
+tm_raster(title='Elevation')+
 tm_shape(liberia.adm0)+
 tm_borders(lwd=2)
 
 liberia.alt <- mask(liberia.alt,
-                    as(liberia.adm0,"Spatial"))
+                    as(liberia.adm0,'Spatial'))
 
 tm_shape(liberia.alt)+
-tm_raster(title="Elevation (m)")+
+tm_raster(title='Elevation (m)')+
 tm_shape(liberia.adm0)+
 tm_borders(lwd=2)
 
@@ -75,7 +86,7 @@ rb$my_elevation <- extract(liberia.alt,rb.sf)
 # The grid I am creating has a resolution of 2 by 2 km
 liberia.grid <- st_make_grid(liberia.adm0,
                              cellsize = 2000,
-                             what="centers")
+                             what='centers')
 
 # I am subsetting only the grid locations that fall inside Liberia
 liberia.inout <- st_intersects(liberia.grid,
@@ -90,18 +101,18 @@ dist <- apply(st_distance(liberia.grid,
 dist.raster <- rasterFromXYZ(cbind(
   st_coordinates(liberia.grid),
   dist),
-  crs="+init=epsg:32629")
+  crs='+init=epsg:32629')
 
 tm_shape(dist.raster)+
-  tm_raster(title="Distance from \n closest waterway (km)")+
+  tm_raster(title='Distance from \n closest waterway (km)')+
   tm_shape(liberia.adm0)+
   tm_borders(lwd=2)+
   tm_shape(liberia.wl)+
-  tm_lines(col="blue",lwd=2)
+  tm_lines(col='blue',lwd=2)
 
 writeRaster(dist.raster,
-            "Liberia_dist_cw.tiff",
-            "GTiff")
+            'Liberia_dist_cw.tiff',
+            'GTiff')
 
 liberia.max2km.dist <- 
 extract(dist.raster,
@@ -112,7 +123,7 @@ extract(dist.raster,
 dist.raster2 <- rasterFromXYZ(cbind(
   st_coordinates(liberia.grid),
   liberia.max2km.dist),
-  crs="+init=epsg:32629")
+  crs='+init=epsg:32629')
 plot(dist.raster2)
 
 plot(dist.raster2 )
@@ -120,7 +131,7 @@ plot(dist.raster2 )
 
 
 ### Not necessary for the course but useful to know
-liberia.adm2 <- st_read("Liberia spatial data/LBR_adm/LBR_adm2.shp")
+liberia.adm2 <- st_read('Liberia spatial data/LBR_adm/LBR_adm2.shp')
 liberia.adm2 <- st_transform(liberia.adm2,crs=32629)
 
 names.adm2 <- liberia.adm2$NAME_2
@@ -134,7 +145,7 @@ for(i in 1:n.adm2) {
   elev.r.i <- mask(liberia.alt,
                     as(
                     liberia.adm2[ind.sel,],
-                    "Spatial"))
+                    'Spatial'))
   liberia.adm2$Mean_elevation[ind.sel] <- 
     mean(values(elev.r.i),na.rm=TRUE)
 }
@@ -143,6 +154,6 @@ for(i in 1:n.adm2) {
 map2 <- tm_shape(liberia.adm2) + 
   tm_borders(lwd=1) 
 
-map2+tm_fill("Mean_elevation",
-             title = "Mean elevation (m)")
+map2+tm_fill('Mean_elevation',
+             title = 'Mean elevation (m)')
 
