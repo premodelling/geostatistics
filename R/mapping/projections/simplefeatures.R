b45559e4ecbf5894d8079a5388045237cce009ac
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-03 00:09:20 +0100

    Update R/mapping/projections/simplefeatures.R

--- R/mapping/projections/simplefeatures.R
+++ R/mapping/projections/simplefeatures.R
@@ -4,35 +4,34 @@
 # Created on: 29/04/2022
 
 
-#' Creating an sf class object
-#' the sf class: geometries + non-geographic attributes
+
+#' Simple Features
 #'
+#' Herein:
+#'      Creating a simple features class object
+#'      Note that a sf class is a combination of ↠ geometries & non-geographic attributes
+#'
+
 
 # simple features geometry object
 london_point <- sf::st_point(x = c(0.1, 51.5))
 class(london_point)
+
 
 # simple features columns object
 london_geometry <- st_sfc(london_point) %>%
   st_set_crs(value = 'EPSG:4326')
 class(london_geometry)
 
+
 # data.frame object
 london_attributes <- data.frame(
   name = 'London', temperature = 25, date = as.Date(x = '2017-06-21'))
 class(london_attributes)
 
-# simple feature
+
+# a <simple feature class> object
 london_sf <- sf::st_sf(london_attributes, geometry = london_geometry)
 class(london_sf)
-
-
-
-
-
-
-
-
-
 
 
