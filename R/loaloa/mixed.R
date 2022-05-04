ec812721497f08f449ccd79bed5bbfb2e5fcf4f2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-04 21:52:10 +0100

    Add R/loaloa/mixed.R

---
+++ R/loaloa/mixed.R
@@ -0,0 +1,60 @@
+# Title     : mixed.R
+# Objective : Mixed models
+# Created by: greyhypotheses
+# Created on: 04/05/2022
+
+
+
+# data
+data(loaloa)
+str(object = loaloa)
+coordinates <- loaloa[, c('LONGITUDE', 'LATITUDE')]
+
+
+# geography
+loaloa <- loaloa %>% st_as_sf(coords = c('LONGITUDE', 'LATITUDE')) %>%
+  st_set_crs(value = 'EPSG:4326')
+loaloa <- st_transform(loaloa, crs = 32632)
+
+loaloa$utm_x <- as.numeric(st_coordinates(loaloa, UTM)[, 1])
+loaloa$utm_y <- as.numeric(st_coordinates(loaloa, UTM)[, 2])
+
+loaloa$geometry <- NULL
+loaloa <- base::merge(x = loaloa, y = coordinates)
+
+
+# 5. index level random intercept
+model <- glmer(formula = cbind(NO_INF, NO_EXAM - NO_INF) ~ MEAN9901 + (1|ROW),
+               family = binomial(link = 'logit'), data = loaloa)
+estimates <- summary(model)
+effects <- ranef(object = model)
+
+# ... sigma^2
+sigmasqr <- attr(estimates$varcor['ROW']$ROW, which = 'stddev')^2
+
+
+
+# 6
+spat.corr.diagnostic(NO_INF ~ MEAN9901,
+                     units.m = ~ NO_EXAM,
+                     coords = ~ I(utm_x/1000) + I(utm_y/1000),
+                     data = loaloa,
+                     ID.coords = loaloa$ROW,
+                     likelihood = 'Binomial')
+
+effects.intercept <- effects$ROW$`(Intercept)`
+frame <- data.frame(intercept = effects.intercept, easting = loaloa$utm_x, northing = loaloa$utm_y)
+effects.variogram <- variogram(frame, ~ intercept, ~ easting + northing)
+plot(effects.variogram, typ = 'b', frame.plot = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
