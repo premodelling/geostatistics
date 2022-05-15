7443e7d2e2d17df1026bdecfc182546c47f2d1d7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-15 20:07:53 +0100

    Update docs/GEO/AreaGrid.R

--- docs/GEO/AreaGrid.R
+++ docs/GEO/AreaGrid.R
@@ -14,18 +14,3 @@ BasicAreaGrid <- function (area) {
   return(points)
 
 }
-
-AlternativeAreaGrid <- function (area) {
-
-  points <- st_make_grid(area, cellsize = 2000, what = 'centers')
-
-  liberia.inside <- st_intersects(points, area, sparse = FALSE)
-  points <- points[liberia.inside]
-  frame <- data.frame(X = as.numeric(st_coordinates(points)[, 1]),
-                      Y = as.numeric(st_coordinates(points)[, 2])) %>%
-    st_as_sf(coords = c('X', 'Y'))
-  st_crs(frame) <- st_crs(area)$srid
-
-  return(frame)
-
-}
