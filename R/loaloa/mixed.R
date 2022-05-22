d37e60a569eee6133958a122bb7678b53873032c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-05-22 03:16:50 +0100

    Update R/loaloa/mixed.R

--- R/loaloa/mixed.R
+++ R/loaloa/mixed.R
@@ -19,8 +19,21 @@ loaloa <- st_transform(loaloa, crs = 32632)
 loaloa$utm_x <- as.numeric(st_coordinates(loaloa, UTM)[, 1])
 loaloa$utm_y <- as.numeric(st_coordinates(loaloa, UTM)[, 2])
 
-loaloa$geometry <- NULL
-loaloa <- base::merge(x = loaloa, y = coordinates)
+
+# What?
+# loaloa$geometry <- NULL
+# loaloa <- base::merge(x = loaloa, y = coordinates)
+
+
+# Aside.
+loaloa$logit <- log((loaloa$NO_INF + 0.5)/(loaloa$NO_EXAM - loaloa$NO_INF + 0.5))
+baseline <- lm(logit ~ ELEVATION + I(ELEVATION^2), data = st_drop_geometry(loaloa))
+variograms <- variog(
+  coords = loaloa %>% dplyr::select('utm_x', 'utm_y') %>% st_drop_geometry() %>% dplyr::select('utm_x', 'utm_y')/1000,
+  data = residuals(baseline))
+
+
+
 
 
 # 5. index level random intercept
@@ -35,12 +48,12 @@ sigmasqr <- attr(estimates$varcor['ROW']$ROW, which = 'stddev')^2
 
 
 # 6
-spat.corr.diagnostic(NO_INF ~ MEAN9901,
-                     units.m = ~ NO_EXAM,
-                     coords = ~ I(utm_x/1000) + I(utm_y/1000),
-                     data = loaloa,
-                     ID.coords = loaloa$ROW,
-                     likelihood = 'Binomial')
+diagnostics <- spat.corr.diagnostic(NO_INF ~ MEAN9901,
+                                    units.m = ~NO_EXAM,
+                                    coords = ~I(utm_x / 1000) + I(utm_y / 1000),
+                                    data = loaloa,
+                                    ID.coords = loaloa$ROW,
+                                    likelihood = 'Binomial', lse.variogram = TRUE)
 
 effects.intercept <- effects$ROW$`(Intercept)`
 frame <- data.frame(intercept = effects.intercept, easting = loaloa$utm_x, northing = loaloa$utm_y)
