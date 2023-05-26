3d14d100747dde97bbc85583f9cee9427c53264c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-26 16:00:38 +0100

    Update R/algorithms/Unlink.R

--- R/algorithms/Unlink.R
+++ R/algorithms/Unlink.R
@@ -4,6 +4,12 @@
 # Created on: 29/06/2022
 
 
+#' Unlink Directories
+#'
+#' @description Deletes a directory recursively
+#'
+#' @param path: A directory path string
+#'
 UnlinkDirectories <- function (path) {
 
   if (dir.exists(paths = path)) {
@@ -13,6 +19,12 @@ UnlinkDirectories <- function (path) {
 }
 
 
+#' Unlink Files
+#'
+#' @description Deletes a file
+#'
+#' @param path: A file's path & file name string, including the file name's extension
+#'
 UnlinkFiles <- function (path) {
 
   if (file.exists(base::dirname(path = path))) {
