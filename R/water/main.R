1654c08427969b03e8b870b0a3e9f1703a140021
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-06 18:08:50 +0100

    Update R/water/main.R

--- R/water/main.R
+++ R/water/main.R
@@ -6,24 +6,31 @@
 
 rm(list = ls())
 
+
+#' Segments
+#'
+#'
+
 # functions
 source(file = file.path(getwd(), 'R', 'algorithms', 'encoding', 'UTM.R'))
 source(file = file.path(getwd(), 'R', 'algorithms', 'encoding', 'Geocoding.R'))
-
-
-
 
 # setting the reference coordinates
 degrees <- AddressGeocoding(address = 'England')
 utm <- UTM(longitude = degrees$longitude, latitude = degrees$latitude)
 
+# data
+source(file = file.path(getwd(), 'R', 'water', 'SegmentsData.R'))
+frame <- SegmentsData(utm = utm)
 
+# catchments
 catchments <- c('anglian', 'humber', 'thames')
 
 
 
-
-
+#' Discharges
+#'
+#'
 source(file = file.path(getwd(), 'R', 'water', 'Discharges.R'))
 Discharges()
 
