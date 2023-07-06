0b5fb769930ceb0826b0e32eb725ce42370d0a22
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-06 15:59:33 +0100

    Update R/water/Segments.R

--- R/water/Segments.R
+++ R/water/Segments.R
@@ -13,21 +13,65 @@ source(file = file.path(getwd(), 'R', 'algorithms', 'encoding', 'Geocoding.R'))
 
 Segments <- function () {
 
+
   # setting the reference coordinates
   degrees <- AddressGeocoding(address = 'River Thames, England')
   utm <- UTM(longitude = degrees$longitude, latitude = degrees$latitude)
 
-  # A basin
+
+  # Polygon
   basin <- sf::st_read(dsn = file.path(getwd(),'data', 'shapes', 'catchments', 'thames',
                                        'district', 'WFD_River_Basin_Districts_Cycle_3.shp'))
   diagram <- st_transform(basin, crs = utm)
-
-  # An illustration of the basin
   map <- tm_shape(diagram) +
     tm_layout(main.title = 'Basi', frame = FALSE) +
     tm_borders(lwd = 0.5)
   map
 
 
+  # Coordinates
+  # utils::read.csv()
+  # readr::read_csv()
+  url <- 'https://raw.githubusercontent.com/thirdreading/experiment/develop/warehouse/hydrometry/references/gazetteer.csv'
+  stations <- data.table::fread(url, header = TRUE, encoding = 'UTF-8')
+
+
+  # setting the reference coordinates
+  frame <- st_as_sf(stations, coords = c('longitude', 'latitude')) %>%
+    st_set_crs(value = 'EPSG:4326')
+  frame$longitude <- stations$longitude
+  frame$latitude <- stations$latitude
+
+
+  # transforming
+  frame <- st_transform(frame, crs = paste0('EPSG:', utm))
+  frame$utm_x <- as.numeric(st_coordinates(frame, UTM)[, 1])
+  frame$utm_y <- as.numeric(st_coordinates(frame, UTM)[, 2])
+
+
+
+  # ...
+  T <- st_intersects(frame, diagram, sparse = FALSE)
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
