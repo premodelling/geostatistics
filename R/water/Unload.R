702c25497b58bc20c56ce9aef9269488743188f0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-07 05:50:53 +0100

    Update R/water/Unload.R

--- R/water/Unload.R
+++ R/water/Unload.R
@@ -8,18 +8,18 @@
 #'
 #' @note https://environment.data.gov.uk/catchment-planning
 #'
-#' @param number: A `river basin district` catchment number
-#' @param text: The catchment's name
+#' @param number: A `river basin district` number
+#' @param district: The district's name
 #'
 #'
-Unload <- function (number, text) {
+Unload <- function (number, district) {
 
   url <- glue::glue('https://environment.data.gov.uk/catchment-planning/RiverBasinDistrict/{as.character(number)}/shapefile.zip')
 
   temp <- base::tempfile()
   utils::download.file(url = url, temp)
   utils::unzip(temp, files = NULL, list = FALSE, overwrite = TRUE,
-               junkpaths = FALSE, exdir = file.path(getwd(), 'data', 'shapes', 'catchments', text),
+               junkpaths = FALSE, exdir = file.path(getwd(), 'data', 'shapes', 'basin', 'districts', district),
                unzip = "internal", setTimes = FALSE)
   unlink(temp)
 
