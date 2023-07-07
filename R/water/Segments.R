17d80e302d743cff25c3db78101d0af1ad96a39b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-07 06:11:12 +0100

    Add R/water/Segments.R

--- R/water/Segments.R
+++ R/water/Segments.R
@@ -7,14 +7,16 @@
 
 #' Segments
 #'
-#' @param catchment
-#' @param utm
+#' @param district: The name of a river basin  district
+#' @param frame: Which records of <frame> lie within the district?
+#' @param utm: Universal Transverse Mercator code
 #'
-Segments <- function (catchment, frame, utm) {
+Segments <- function (district, frame, utm) {
+
 
   # Polygon
-  basin <- sf::st_read(dsn = file.path(getwd(),'data', 'shapes', 'catchments', catchment,
-                                       'districts', 'WFD_River_Basin_Districts_Cycle_3.shp'))
+  basin <- sf::st_read(dsn = file.path(getwd(),'data', 'shapes', 'basin', 'districts', district,
+                                       'WFD_River_Basin_Districts_Cycle_3.shp'))
   diagram <- sf::st_transform(basin, crs = utm)
   map <- tmap::tm_shape(diagram) +
     tmap::tm_layout(main.title = 'Basin', frame = FALSE) +
@@ -22,12 +24,15 @@ Segments <- function (catchment, frame, utm) {
   map
 
 
-  # ...
+  # Points of <frame> within <diagram>
   T <- sf::st_intersects(frame, diagram, sparse = FALSE)
   frame$place <- as.integer(T)
-  colnames(frame)[colnames(frame) == 'place'] <- catchment
+  colnames(frame)[colnames(frame) == 'place'] <- district
 
-  reduced <- frame[, c('station_id', catchment)]
+
+  # Focusing on firld <station_id>, and the district field, only.
+  reduced <- frame[, c('station_id', district)]
+
 
   return (sf::st_drop_geometry(reduced))
 
